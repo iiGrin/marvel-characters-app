efd2a3f56c08290735ec76911ab3568719073d51
devIggy <devdevgrin@gmail.com>
2024-10-22 13:47:41 +0300

    feat: header layout and styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Header from "./components/header/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
